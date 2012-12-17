3ebc93d263f5bca65001433fe3b87e76b57bb480
Michael Alt <mail@michael-alt.info>
2012-12-17 13:04:16 +0100

    Lesson 02: Arrays

---
+++ 02_arrays/script.js
@@ -0,0 +1,13 @@
+
+var primes = [ 2, 3, 5, 7, 11, 13 ];
+
+log(primes);
+log(typeof primes);
+
+var fibos =  new Array(0,1,1,2,3,5,8,13);
+
+log(fibos);
+log(typeof fibos);
+
+log(fibos.push(21));
+log(fibos);
