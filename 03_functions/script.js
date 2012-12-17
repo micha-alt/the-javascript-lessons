c50a46e8e2a09d50875af4bdcb8f428fe9b35089
Michael Alt <mail@michael-alt.info>
2012-12-17 13:13:41 +0100

    Lesson 03: Functions

---
+++ 03_functions/script.js
@@ -0,0 +1,20 @@
+
+function add( a, b ) {
+    return a + b;
+}
+
+var res = add(17,4);
+
+function sum () {
+    var summe = 0;
+    for( i=0; i<arguments.length; i++) {
+        summe += arguments[i];
+    }
+    return summe;
+};
+
+log("Ergebnis:",sum( 1,2,3));
+
+log(add.length);
+log(add.apply(this, [ 23, 19 ]));
+
